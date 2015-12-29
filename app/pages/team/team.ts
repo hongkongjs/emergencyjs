11fc9273ecf911131ef99348e403652414382f9e
zakir2k <zakir.burki@gmail.com>
2015-12-30 01:45:17 +0800

    hack way to get segment switcher working

--- app/pages/team/team.ts
+++ app/pages/team/team.ts
@@ -6,7 +6,18 @@ import {Page, NavController} from 'ionic-framework/ionic';
 })
 export class TeamPage {
   constructor(nav: NavController) {
-  	this.team = "grid";
+	this.team = 'grid';
+	this.isGrid = true;
+	this.isList = false;
   }
-
+  switch(segment) { 
+  	this.team = segment;
+	 if (segment == 'grid') {
+	 	this.isGrid = true;
+	 	this.isList = false;
+	 }else{
+	 	this.isGrid = false;
+	 	this.isList = true;
+	 }
+  }
 }
