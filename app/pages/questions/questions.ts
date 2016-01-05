f7eee5d759df4e703863808c83de39c704d7ad7f
Eumir Gonzales <e.gonzales@codeshop.io>
2016-01-05 18:37:44 +0800

    Adds blank questions detail page, adds navigation to said page, and deletes unused code

--- app/pages/questions/questions.ts
+++ app/pages/questions/questions.ts
@@ -1,5 +1,5 @@
 import {IonicApp, Page, NavController, NavParams} from 'ionic-framework/ionic';
-
+import {QuestionsDetail} from './questionsdetail';
 
 @Page({
   templateUrl: 'build/pages/questions/questions.html'
@@ -7,26 +7,10 @@ import {IonicApp, Page, NavController, NavParams} from 'ionic-framework/ionic';
 export class QuestionsPage {
   constructor(app: IonicApp, nav: NavController, navParams: NavParams) {
     this.nav = nav;
-
-    // If we navigated to this page, we will have an item available as a nav param
-    this.selectedItem = navParams.get('item');
-
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
-
-    this.items = [];
-    for(let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
   }
 
-  itemTapped(event, item) {
-    this.nav.push(QuestionsPage, {
-      item: item
-    })
+  itemTapped(item) {
+    this.nav.push(QuestionsDetail, { item: item })
+    console.log("switch screen");
   }
 }
