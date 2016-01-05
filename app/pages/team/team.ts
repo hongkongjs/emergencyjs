32879e7e96835f6cb4347855f67137d2b32db21f
zakir2k <zakir.burki@gmail.com>
2016-01-05 09:52:25 +0800

    grid-list switcher fixed by ionic2 team

--- app/pages/team/team.ts
+++ app/pages/team/team.ts
@@ -7,17 +7,5 @@ import {Page, NavController} from 'ionic-framework/ionic';
 export class TeamPage {
   constructor(nav: NavController) {
 	this.team = 'grid';
-	this.isGrid = true;
-	this.isList = false;
-  }
-  switch(segment) { 
-  	this.team = segment;
-	 if (segment == 'grid') {
-	 	this.isGrid = true;
-	 	this.isList = false;
-	 }else{
-	 	this.isGrid = false;
-	 	this.isList = true;
-	 }
   }
 }
