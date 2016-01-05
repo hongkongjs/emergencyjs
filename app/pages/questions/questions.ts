44926d66750e9d86f463e4d6f0fbe449cf8d0899
Eumir Gonzales <e.gonzales@codeshop.io>
2016-01-05 19:28:37 +0800

    adds modal functionality, adds modal html file

--- app/pages/questions/questions.ts
+++ app/pages/questions/questions.ts
@@ -1,16 +1,36 @@
-import {IonicApp, Page, NavController, NavParams} from 'ionic-framework/ionic';
+import {IonicApp, Page, NavController, NavParams, Modal, Platform} from 'ionic-framework/ionic';
 import {QuestionsDetail} from './questionsdetail';
 
 @Page({
   templateUrl: 'build/pages/questions/questions.html'
 })
 export class QuestionsPage {
-  constructor(app: IonicApp, nav: NavController, navParams: NavParams) {
+
+  constructor(app: IonicApp, nav: NavController, navParams: NavParams, modal: Modal) {
     this.nav = nav;
+    this.modal = modal;
   }
 
   itemTapped(item) {
     this.nav.push(QuestionsDetail, { item: item })
     console.log("switch screen");
   }
+
+  openModal(item){
+    this.modal.open(questionModal);
+    console.log("Open Modal");
+  }
+
+}
+
+@Page({
+  templateUrl: 'build/pages/questions/questionadd.html'
+})
+class questionModal{
+
+  constructor(modal: Modal) {
+    this.nav = nav;
+    this.modal = Modal;
+  }
+  
 }
