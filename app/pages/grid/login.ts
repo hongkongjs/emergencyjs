fbaf9024ac3ccde08f8ed508c429f14be4349f0d
Connie Leung <cleung334@gmail.com>
2016-01-14 22:31:06 +0800

    Add navigation to navigate between sign up page, reset password page and login page.

--- app/pages/grid/login.ts
+++ app/pages/grid/login.ts
@@ -1,5 +1,7 @@
+import {NavController} from 'ionic-framework/ionic';
 import {Page} from 'ionic-framework/ionic';
 import {OnInit} from 'angular2/core';
+import {ResetPage} from './reset';
 
 @Page({
   templateUrl: 'build/pages/grid/login.html'
@@ -8,7 +10,19 @@ export class LoginPage implements OnInit {
 
   public disabled : boolean;
 
+  constructor(nav: NavController) {
+    this.nav = nav;
+  }
+
   ngOnInit() {
       this.disabled = true;
+  }
+
+  gotoSignup() {
+    this.nav.pop();
+  }
+
+  gotoResetPassword() {
+    this.nav.push(ResetPage, {});
   }
 }
--- app/pages/grid/signup.ts
+++ app/pages/grid/signup.ts
@@ -1,5 +1,7 @@
+import {NavController} from 'ionic-framework/ionic';
 import {Page} from 'ionic-framework/ionic';
 import {OnInit} from 'angular2/core';
+import {LoginPage} from './login';
 
 @Page({
   templateUrl: 'build/pages/grid/signup.html'
@@ -8,11 +10,16 @@ export class SignUpPage implements OnInit {
 
   public disabled: boolean;
 
+  constructor(nav: NavController) {
+    this.nav = nav;
+  }
+
   ngOnInit() {
     this.disabled = true;
   }
 
   gotoLogin() {
     console.log("suppose to go to login page");
+    this.nav.push(LoginPage, {});
   }
 }
