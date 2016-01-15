5f5f46cd456b4ea409d59c348c8b0dea8552acfc
Connie Leung <cleung334@gmail.com>
2016-01-16 02:25:54 +0800

    Listen key event and enable/disable signup button.

---
+++ app/pages/grid/models.ts
@@ -0,0 +1,7 @@
+export interface User {
+  username: string,
+  firstName: string,
+  lastName: string,
+  email: string,
+  password: string
+}
--- app/pages/grid/signup.ts
+++ app/pages/grid/signup.ts
@@ -1,25 +1,44 @@
 import {NavController} from 'ionic-framework/ionic';
 import {Page} from 'ionic-framework/ionic';
-import {OnInit} from 'angular2/core';
 import {LoginPage} from './login';
+import {User} from './models';
 
 @Page({
   templateUrl: 'build/pages/grid/signup.html'
 })
-export class SignUpPage implements OnInit {
+export class SignUpPage {
 
   public disabled: boolean;
+  public user: User;
 
   constructor(nav: NavController) {
     this.nav = nav;
+    this.user = { 'username' : '', 'firstName': '', 'lastName' : '',
+                  'email': '', 'password': ''};
+    this.disabled = true;
   }
 
-  ngOnInit() {
-    this.disabled = true;
+  checkChanges(changeData: any) {
+    console.log('name: ' + changeData.name + ', value: ' + changeData.value);
+
+//    let valid = new RegExp('/^(("[\w-+\s]+")|([\w-+]+(?:\.[\w-+]+)*)|("[\w-+\s]+")([\w-+]+(?:\.[\w-+]+)*))(@((?:[\w-+]+\.)*\w[\w-+]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][\d]\.|1[\d]{2}\.|[\d]{1,2}\.))((25[0-5]|2[0-4][\d]|1[\d]{2}|[\d]{1,2})\.){2}(25[0-5]|2[0-4][\d]|1[\d]{2}|[\d]{1,2})\]?$)/i');
+    let newUser = this.user.username;
+    let newfirstName = this.user.firstName;
+    let newlastName = this.user.lastName;
+    let newEmail = this.user.email;
+    let newPassword = this.user.password;
+
+//    let validEmail =!!valid.test(newEmail);
+    this.disabled = !(newUser && newfirstName && newlastName && newEmail && newPassword);
+    console.log('disabled = ' + this.disabled);
   }
 
   gotoLogin() {
     console.log("suppose to go to login page");
     this.nav.push(LoginPage, {});
+  }
+
+  getDiagnostics() {
+    return JSON.stringify(this.user);
   }
 }
